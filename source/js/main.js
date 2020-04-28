a3391e299e848b8fcec468798ceaf167d471bac7
July-1390 <63366643+July-1390@users.noreply.github.com>
2020-04-28 21:14:59 +0200

    update main.js
    add functional for .hero-carousel

--- source/js/main.js
+++ source/js/main.js
@@ -24,6 +24,27 @@ $(function() {
     $("#bestSellerCarousel").owlCarousel(owlSettings);
 })
 
+$(".hero-carousel").owlCarousel({
+    items: 3,
+    margin: 10,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        810: {
+            items: 3
+        }
+    }
+});
+
 
 
 // ==== Active Nice Select ==== //
